870e980d634d80aff90546e2aebbf498809df7ca
James Sutton <james@algorithmia.com>
2017-12-20 12:57:57 -0800

    renamed 'data' to 'data_path' for my sanity

--- src/GenerativeForecast.py
+++ src/GenerativeForecast.py
@@ -6,7 +6,7 @@ from src.modules import data_proc, graph, misc, net_misc, envelope
 class InputGuard():
     def __init__(self):
         self.mode = None
-        self.data = None
+        self.data_path = None
         self.checkpoint_input_path = None
         self.checkpoint_output_path = None
         self.image_save_path = None
@@ -30,10 +30,10 @@ def process_input(input):
         else:
             raise misc.AlgorithmError("mode is invalid, must be 'forecast', or 'train'")
     if guard.mode == "train":
-        if 'data' in input:
-            guard.data = type_check(input, 'data', str)
+        if 'data_path' in input:
+            guard.data_path = type_check(input, 'data_path', str)
         else:
-            raise misc.AlgorithmError("'data' required for 'train' mode")
+            raise misc.AlgorithmError("'data_path' required for 'train' mode")
         if 'checkpoint_input_path' in input:
             guard.checkpoint_input_path = type_check(input, 'checkpoint_input_path', str)
         if 'checkpoint_output_path' in input:
@@ -53,8 +53,8 @@ def process_input(input):
         if 'io_noise' in input:
                 guard.io_noise = type_check(input, 'io_noise', float)
     elif guard.mode == "forecast":
-        if 'data' in input:
-            guard.data = type_check(input, 'data', str)
+        if 'data_path' in input:
+            guard.data_path = type_check(input, 'data_path', str)
         if 'checkpoint_input_path' in input:
             guard.checkpoint_input_path = type_check(input, 'checkpoint_input_path', str)
         else:
@@ -88,9 +88,9 @@ def apply(input):
     if input['mode'] == "forecast":
         local_file = data_proc.get_file(guard.checkpoint_input_path)
         network, state = net_misc.load_checkpoint(local_file)
-        if guard.data:
-            guard.data = data_proc.get_frame(guard.data)
-            guard.data = data_proc.process_frames_incremental(guard.data, state, outlier_removal_multiplier)
+        if guard.data_path:
+            guard.data_path = data_proc.get_frame(guard.data_path)
+            guard.data_path = data_proc.process_frames_incremental(guard.data_path, state, outlier_removal_multiplier)
 
         forecasts, state, checkpoint_model = net_misc.create_forecasts(guard.data, network, state, guard.iterations, guard.forecast_size, guard.io_noise)
 
@@ -105,14 +105,14 @@ def apply(input):
         formatted_envelope = envelope.ready_envelope(env, state['io_width'])
         output['envelope'] = formatted_envelope
     if guard.mode == "train":
-        guard.data = data_proc.get_frame(guard.data)
+        guard.data_path = data_proc.get_frame(guard.data_path)
         if guard.checkpoint_input_path:
             local_file = data_proc.get_file(guard.checkpoint_input_path)
             network, state = net_misc.load_checkpoint(local_file)
-            data = data_proc.process_frames_incremental(guard.data, state, gradient_multiplier)
+            data = data_proc.process_frames_incremental(guard.data_path, state, gradient_multiplier)
             lr_rate = net_misc.determine_lr(data, state)
         else:
-            data, norm_boundaries = data_proc.process_frames_initial(guard.data, outlier_removal_multiplier, beam_width=guard.future_beam_width)
+            data, norm_boundaries = data_proc.process_frames_initial(guard.data_path, outlier_removal_multiplier, beam_width=guard.future_beam_width)
             io_dim = len(norm_boundaries)
             learning_rate = float(base_learning_rate) / io_dim
             network, state = net_misc.initialize_network(io_dim=io_dim, layer_width=guard.layer_width, max_history=max_history,
@@ -133,16 +133,16 @@ def test_train():
     input = dict()
     input['mode'] = "train"
     # input['mode'] = "forecast"
-    # input['data'] = "data://zeryx/forecasting_testing/rossman_training_2.csv"
-    # input['data'] = 'data://zeryx/forecasting_testing/sinewave_bulk.csv'
-    # input['data'] = 'data://zeryx/forecasting_testing/sinewave_incremental.csv'
-    input['data'] = 'data://zeryx/forecasting_testing/rossman_training_2.csv'
-    # input['data'] = 'data://zeryx/forecasting_testing/csdisco-train_1.csv'
-    # input['data'] = 'data://zeryx/forecasting_testing/sine_wave_train.csv'
-    # input['data'] = "data://zeryx/forecasting_testing/funpokes-train.csv"
-    # input['data'] = "data://zeryx/forecasting_testing/funpokes-test.csv"
-    # input['data'] = "data://zeryx/forecasting_testing/csdisco_2.csv"
-    # input['data'] = "data://zeryx/forecasting_testing/csdisco_3.csv"
+    # input['data_path'] = "data://zeryx/forecasting_testing/rossman_training_2.csv"
+    # input['data_path'] = 'data://zeryx/forecasting_testing/sinewave_bulk.csv'
+    # input['data_path'] = 'data://zeryx/forecasting_testing/sinewave_incremental.csv'
+    input['data_path'] = 'data://zeryx/forecasting_testing/rossman_training_2.csv'
+    # input['data_path'] = 'data://zeryx/forecasting_testing/csdisco-train_1.csv'
+    # input['data_path'] = 'data://zeryx/forecasting_testing/sine_wave_train.csv'
+    # input['data_path'] = "data://zeryx/forecasting_testing/funpokes-train.csv"
+    # input['data_path'] = "data://zeryx/forecasting_testing/funpokes-test.csv"
+    # input['data_path'] = "data://zeryx/forecasting_testing/csdisco_2.csv"
+    # input['data_path'] = "data://zeryx/forecasting_testing/csdisco_3.csv"
     # input['checkpoint_output_path'] = "data://zeryx/forecasting_testing/rossman_model.t7"
     # input['checkpoint_output_path'] = "data://zeryx/forecasting_testing/sinewave_bulk_model.t7"
     # input['checkpoint_output_path'] = "data://zeryx/forecasting_testing/sinewave_incremented_model.t7"
@@ -165,13 +165,13 @@ def test_forecast():
     input = dict()
     input['mode'] = "forecast"
     input['checkpoint_input_path'] = "data://zeryx/forecasting_testing/sinewave_incremented_model.t7"
-    # input['data'] = "data://zeryx/forecasting_testing/funpokes-test.csv"
-    # input['data'] = 'data://zeryx/forecasting_testing/sinewave_eval.csv'
-    # input['data'] = 'data://zeryx/forecasting_testing/csdisco-test_2.csv'
+    # input['data_path'] = "data://zeryx/forecasting_testing/funpokes-test.csv"
+    # input['data_path'] = 'data://zeryx/forecasting_testing/sinewave_eval.csv'
+    # input['data_path'] = 'data://zeryx/forecasting_testing/csdisco-test_2.csv'
     # input['checkpoint_input_path'] = "data://zeryx/forecasting_testing/sinewave_incremented_model.t7"
     # input['checkpoint_input_path'] = "data://zeryx/forecasting_testing/csdisco_model.t7"
-    # input['data'] = 'data://zeryx/forecasting_testing/btc-test_2.csv'
-    # input['data'] = 'data://zeryx/forecasting_testing/sine_wave_test.csv'
+    # input['data_path'] = 'data://zeryx/forecasting_testing/btc-test_2.csv'
+    # input['data_path'] = 'data://zeryx/forecasting_testing/sine_wave_test.csv'
     # input['checkpoint_input_path'] = "data://zeryx/forecasting_testing/csdisco_model.t7"
     # input['checkpoint_output_path'] = "data://zeryx/forecasting_testing/unavariate_model_up2date.t7"
     # input['checkpoint_input_path'] = "data://zeryx/forecasting_testing/btc_up2date_model.t7"
