cc2f2aa210f6f7c2dc10eb69573abfb33ae4ee93
James Sutton <james@algorithmia.com>
2017-12-29 09:16:04 -0800

    initial no_hack branch commit, using for testing

--- src/GenerativeForecast.py
+++ src/GenerativeForecast.py
@@ -1,7 +1,10 @@
 import json
-from src.modules import misc
-import subprocess
-import tempfile, os
+
+import torch
+from src.modules import data_proc, graph, net_misc, envelope, misc
+torch.backends.cudnn.enabled = False
+
+
 class InputGuard():
     def __init__(self):
         self.mode = None
@@ -17,6 +20,10 @@ class InputGuard():
         self.attention_beam_width = 25
         self.future_beam_width = 10
         self.input_dropout = 0.45
+
+    def __init__(self, dictionary):
+        for key in dictionary:
+            setattr(self, key, dictionary[key])
 
 
 
@@ -78,33 +85,74 @@ def type_check(dic, id, type):
     else:
         raise misc.AlgorithmError("'{}' must be of {}".format(str(id), str(type)))
 
+def forecast(guard, outlier_removal_multiplier):
+    output = dict()
+    local_file = data_proc.get_file(guard.checkpoint_input_path)
+    network, state = net_misc.load_checkpoint(local_file)
+    if guard.data_path:
+        guard.data_path = data_proc.get_frame(guard.data_path)
+        guard.data_path = data_proc.process_frames_incremental(guard.data_path, state,
+                                                               outlier_removal_multiplier)
+
+    normal_forecast, raw_forecast, state = net_misc.create_forecasts(guard.data_path, network, state,
+                                                                     guard.iterations, guard.forecast_size,
+                                                                     guard.io_noise)
+
+    output_env = envelope.create_envelope(normal_forecast, guard.forecast_size, state)
+    if guard.graph_save_path:
+        graphing_env = envelope.create_envelope(raw_forecast, guard.forecast_size, state)
+        graph_path = graph.create_graph(graphing_env, state, guard.forecast_size, guard.io_noise)
+        output['saved_graph_path'] = graph.save_graph(graph_path, guard.graph_save_path)
+    if guard.checkpoint_output_path:
+        output['checkpoint_output_path'] = net_misc.save_model(network, guard.checkpoint_output_path)
+    formatted_envelope = envelope.ready_envelope(output_env, state)
+    output['envelope'] = formatted_envelope
+    return output
+
+def train(guard, max_history, base_learning_rate, outlier_removal_multiplier, gradient_multiplier):
+    output = dict()
+    guard.data_path = data_proc.get_frame(guard.data_path)
+    if guard.checkpoint_input_path:
+        local_file = data_proc.get_file(guard.checkpoint_input_path)
+        network, state = net_misc.load_checkpoint(local_file)
+        data = data_proc.process_frames_incremental(guard.data_path, state, outlier_removal_multiplier)
+        lr_rate = net_misc.determine_lr(data, state)
+    else:
+        data, norm_boundaries, headers = data_proc.process_frames_initial(guard.data_path,
+                                                                          outlier_removal_multiplier,
+                                                                          beam_width=guard.future_beam_width)
+        io_dim = len(norm_boundaries)
+        learning_rate = float(base_learning_rate) / io_dim
+        network, state = net_misc.initialize_network(io_dim=io_dim, layer_width=guard.layer_width,
+                                                     max_history=max_history,
+                                                     initial_lr=learning_rate, lr_multiplier=gradient_multiplier,
+                                                     io_noise=guard.io_noise,
+                                                     attention_beam_width=guard.attention_beam_width,
+                                                     future_beam_width=guard.future_beam_width, headers=headers)
+        network.initialize_meta(len(data['x']), norm_boundaries)
+        lr_rate = state['prime_lr']
+    error, network = net_misc.train_autogenerative_model(data_frame=data, network=network,
+                                                         checkpoint_state=state, iterations=guard.iterations,
+                                                         learning_rate=lr_rate, epochs=guard.epochs,
+                                                         drop_percentage=guard.input_dropout)
+    output['checkpoint_output_path'] = net_misc.save_model(network, guard.checkpoint_output_path)
+    output['final_error'] = float(error)
+    return output
+
 def apply(input):
     guard = process_input(input)
-    output = execute_workaround(guard)
+    outlier_removal_multiplier = 5
+    max_history = 500
+    base_learning_rate = 0.5
+    gradient_multiplier = 1.0
+    if guard.mode == "forecast":
+        output = forecast(guard, outlier_removal_multiplier)
+    else:
+        output = train(guard, max_history, base_learning_rate, outlier_removal_multiplier, gradient_multiplier)
+
     return output
 
 
-def execute_workaround(input_data):
-    os.environ['LD_PRELOAD'] = '/usr/lib/x86_64-linux-gnu/libgfortran.so.3'
-    _, in_filename = tempfile.mkstemp()
-    _, out_filename = tempfile.mkstemp()
-    print(in_filename)
-    print(out_filename)
-    with open(in_filename, 'w') as f:
-        json.dump(input_data.__dict__, f)
-    runShellCommand(['python', 'run.py', in_filename, out_filename], cwd=os.path.dirname(os.path.realpath(__file__)))
-    with open(output_queue) as f:
-        output = json.load(f)
-    return output
-
-
-
-def runShellCommand(commands, cwd=None):
-    try:
-        subprocess.check_call(commands, cwd=cwd)
-    except subprocess.CalledProcessError as e:
-        txt = "Error Code:\n" + str(e.returncode) + "\nError output:\n" + str(e.output)
-        raise misc.AlgorithmError(txt)
 
 
 def test_train():
@@ -168,7 +216,7 @@ def test_forecast():
     print(input)
     return apply(input)
 
-# if __name__ == "__main__":
-#   result = test_forecast()
-#   # result = test_train()
-#   print(result)
+if __name__ == "__main__":
+  # result = test_forecast()
+  result = test_train()
+  print(result)
